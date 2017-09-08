7c7b870ffe9b659d4c5cbf785d283d4a4c8008df
Mateusz <mateusz@Kingas-MacBook-Air.local>
2017-09-08 11:23:02 +0200

    add config, dependencies, index files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import persistState from 'redux-localstorage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers';
+
+//Material-UI config
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+//Components imported
+
+injectTapEventPlugin();
+
+const createStoreWithMiddleware = composeWithDevTools(
+	persistState(),
+	applyMiddleware()
+)(createStore);
+
+const NoMatch = () =>
+	<div>
+		<h3>Sorry, page not found</h3>
+	</div>;
+
+ReactDOM.render(
+	<MuiThemeProvider muiTheme={getMuiTheme()}>
+		<Provider store={createStoreWithMiddleware(reducers)}>
+			<BrowserRouter>
+				<div>
+					<Switch>
+						<Route component={NoMatch} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</Provider>
+	</MuiThemeProvider>,
+	document.getElementById('root')
+);
